b4ef4b5dd0bcbed6cc0324e8f610877cc15d8cfa
Geoffrey Roeder <roeder@cs.toronto.edu>
2017-08-15 13:19:20 -0400

    adds one-thread YelpDataset class

--- data/dataset.py
+++ data/dataset.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from torch.utils.data import Dataset, DataLoader
 import torch
 import numpy as np
@@ -48,66 +49,95 @@ class YelpDataset(Dataset):
     a loaded pickle file is a list of dict where each dict is one example
     """
 
-    def __init__(self, vocab_file, text_file,
+    def __init__(self, vocab_file, fields,
                  files_dir='/ais/gobi5/roeder/datasets/yelp_reviews/pickles',
-                 transform=pickle.load):
+                 transform=None):
         self.transform = transform
-        self._files = self._get_filenames(files_dir)
-        self.vocab = self._load_vocab(vocab_file)
-        self.n_batches = len(self.files)
-        self.batch_number = 0
-        self.batch_gen = self._batch_gen(self._files)
-
-        with open(text_file, 'r') as f:
-            self.lines = [line.strip() for line in f]
+        self.fields = fields
+        self._file_names = self._get_filenames(files_dir)
+        self._file_counts = dict()
+        self._n_examples = self._count_file_lines() # side effect: populates self._file_counts
+        self._n_files = len(self._file_names)
+        self._file_idx = 0
+        self._n_seen_prev_files = 0
+        self._example_idx = 0
+        self._curr_file_name = self._file_names[self._file_idx]
+        self._n_lines_curr_file = self._file_counts[self._curr_file_name]
+        self._curr_data = self._load(self._curr_file_name)
+        # self.vocab = self._load_vocab(vocab_file)
 
     def _load_vocab(self, vocab_file):
         vocab = {}
-        ind = 0
-        with open(vocab_file, 'r') as f:
-            for line in f:
+        with open(vocab_file, 'rb') as f:
+            for idx, line in enumerate(f):
                 line = line.strip()
-                self.vocab[line] = ind
-                ind += 1
+                self.vocab[line] = idx
         return vocab
+
+    def _count_lines(self, file):
+        count = 0
+        with open(file, 'rb') as f:
+            f = pickle.load(f)
+            for _ in f:
+                count += 1
+        return count
+
+    def _count_file_lines(self):
+        for file_name in self._file_names:
+            self._file_counts[file_name] = self._count_lines(file_name)
+        return sum(self._file_counts.values())
 
     def _get_filenames(self, files_dir, file_pattern='reviews.pickle.*'):
         return glob.glob(os.path.join(files_dir, file_pattern))
 
-    def _batch_gen(self, files):
-        """ lazily returns the next batch as list of dict"""
-        for file in files:
-            with open(file, "rb") as f:
-                yield self.transform(f)
 
-    def next(self):
-        """ continue generating batches until none left"""
-        try:
-            self.current_batch = next(self.batch_gen)
-            self._process_batch(self)
-            self.batch_number += 1
-            return True
-        except StopIteration:
-            # todo: better way to indicate endpoint?
-            return False
+    def _last_idx(self, file):
+        return len(file) - 1
 
-    def get_batch_text(self):
-        return self._current_text
+    def _load(self, file_name):
+        with open(file_name, 'r') as f:
+            return pickle.load(f)
 
-    def get_batch_json(self):
-        return self._current_json
+    def _load_next(self):
 
-    def _process_batch(self, batch):
-        self._current_text = [json['text'] for json in self.current_batch]
-        self._current_json = self.current_batch
+        # update total seen with lines in previous file
+        self._n_seen_prev_files += self._n_lines_curr_file
+
+        # update references and counts to next file
+        self._file_idx += 1
+        self._curr_file_name = self._file_names[self._file_idx]
+        self._n_lines_curr_file = self._file_counts[self._curr_file_name]
+
+        return self._load(self._curr_file_name)
 
     def __len__(self):
-        """ returns length of current batch"""
-        return len(self.batch)
+        return self._n_examples
 
     def __getitem__(self, idx):
-        # returns text, json from current batch
-        return self._current_text[idx], self._current_json[idx]
+        """ returns line at idx from current data file if any remain,
+            or opens next file and returns first """
+        # convert total data index to current file index
+        curr_file_idx = idx - self._n_seen_prev_files
+
+
+        if curr_file_idx >= self._n_lines_curr_file:
+            # self._curr_data has no more examples
+            print("I am switching from file {} to {}".format(self._file_idx, self._file_idx+1))
+
+            # open and decode new file
+            self._curr_data = self._load_next()
+
+            # correct the index and sanity check
+            curr_file_idx = idx - self._n_seen_prev_files
+            assert(curr_file_idx == 0)
+        print("Batch reads {} using idx {} from file {}".format(idx, curr_file_idx, self._file_idx))
+        print("Current file has {} lines".format(self._n_lines_curr_file))
+
+        line = self._curr_data[curr_file_idx]
+        example = dict()
+        for field in self.fields:
+            example[field] = line[field]
+        return example
 
 
 def pad_batch(examples):
@@ -125,13 +155,13 @@ def pad_batch(examples):
         mask[j, :l] = 1
     return torch.from_numpy(batch), torch.from_numpy(final_inds), torch.from_numpy(mask)
 
-#
-# test_ds = TextDataset("yelp_data/vocab.txt", "yelp_data/part_0")
-# test_loader = DataLoader(test_ds, batch_size=64, shuffle=True, num_workers=4, collate_fn=pad_batch)
-# for batch, lens in test_loader:
-#     print(batch[:4], lens[:4])
-#     print(batch.size(), lens.size())
-#     break
+test_ds = YelpDataset(vocab_file="yelp_data/vocab.txt", fields=['categories','city','cool'], files_dir="./yelp_data/test_pickles")
+# NOTE: preshuffled data. Set shuffle=False or hacked forward read will fail.
+n_batches = 64
+batch_size = len(test_ds) / n_batches
+test_loader = DataLoader(test_ds, batch_size=n_batches, shuffle=False, num_workers=1)
+for batch in test_loader:
+    print(len(batch['categories'][0]))
 
 
 
